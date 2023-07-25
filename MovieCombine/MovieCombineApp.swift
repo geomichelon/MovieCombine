5c2eaf721a660af077c1682732e5eb292bcf6196
George Michelon <geomichelon@gmail.com>
2023-07-25 14:52:10 -0300

    Initial Commit

---
+++ MovieCombine/MovieCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCombineApp.swift
+//  MovieCombine
+//
+//  Created by George Michelon on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
